0b44b157c32c44381e0b00c45271442a0c788312
Johan Erik Pukk <johanerikpukk@gmail.com>
2019-03-31 16:44:01 +0300

    Added basic animations

---
+++ src/Animations.java
@@ -0,0 +1,13 @@
+public class Animations {
+    public static void animateDice(Dice dice) throws InterruptedException {
+        int j = 1;
+        for (int i = 0; i < 50; i++) {
+            System.out.print(j + "\r");
+            if (j == dice.getMax()) {
+                j = 1;
+            } else j++;
+            Thread.sleep(20);
+        }
+        System.out.println(dice.roll());
+    }
+}
--- src/Dice.java
+++ src/Dice.java
@@ -5,9 +5,9 @@ public abstract class Dice {
 
     abstract int getMax();
 
-    static int randIntWithRange(int min, int max){
+    static int randIntWithRange(int min, int max) {
         Random generaator = new Random();
-        return generaator.nextInt(max-min+1)+1;
+        return generaator.nextInt(max - min + 1) + 1;
     }
 
 }
