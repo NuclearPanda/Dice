2e1222d8782f18e268ad2432af8ea8c0656c0cea
Johan Erik Pukk <johanerikpukk@gmail.com>
2019-03-31 17:01:44 +0300

    Fixed animation bug with D10 dice

--- src/Animations.java
+++ src/Animations.java
@@ -2,12 +2,13 @@ public class Animations {
     public static void animateDice(Dice dice) throws InterruptedException {
         int j = 1;
         for (int i = 0; i < 50; i++) {
-            System.out.print(j + "\r");
+            System.out.print(j + "   \r");
             if (j == dice.getMax()) {
                 j = 1;
             } else j++;
             Thread.sleep(20);
         }
+        System.out.print("   \r"); // clear last line
         System.out.println(dice.roll());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Dice a = new D6();
+        Dice a = new D10();
         Animations.animateDice(a);
     }
 }
