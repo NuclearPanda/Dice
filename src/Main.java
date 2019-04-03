0163bdfaddd401561e37cc8fb1b7ed2935c9e979
jaanus91 <jaanusraudsik@hotmail.com>
2019-04-03 16:51:37 +0300

    sorting done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -6,11 +7,15 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Dice a = new D10();
         List<Player> players = createPlayers();
-        //Animations.animateDice(a);
+        Animations.animateDice(a);
 
-
-//        int alustab = whoBegins(players);
+        whoBegins(players);
+        Collections.sort(players);
 //        System.out.println(players.get(alustab));
+        for (Player player :
+                players) {
+            System.out.println(player);
+        }
 
 
     }
@@ -25,7 +30,7 @@ public class Main {
         for (int i = 0; i < playerCount; i++) {
             System.out.print("Sisesta " + (i + 1) + ". mängija nimi: ");
             String name = info.next();
-            players.add(new Player(name, 0));
+            players.add(new Player(name, 0, 0));
         }
         return players;
     }
@@ -37,25 +42,37 @@ public class Main {
         int playerCount = players.size();
         int randIndex = Dice.randIntWithRange(0, playerCount - 1);
         String name = players.get(randIndex).getName();
-        System.out.println(name + " saab otsustada, kes alustab. ");
-        System.out.print("Kas soovid ise alustada? Vasta Jah või Ei: ");
-        Scanner info = new Scanner(System.in);
-        String answer = info.nextLine();
-        System.out.print("\n");
-        if (answer.toLowerCase().equals("jah")) {
-            System.out.println(name + " alustab mängu!");
-        } else if (answer.toLowerCase().equals("ei")) {
-            System.out.println("Valime uue mängija, kes alustab!");
-            whoBegins(players);
-            //TODO Vb võtta välja see kes ütles ei
-        } else {
-            System.out.println("Pole sobiv vastus");
-            whoBegins(players);
+        System.out.println(name + " alustab mängu");
+        for (int i = 0; i < players.size(); i++) {
+            if (i != randIndex) {
+              players.get(i).setQueue(i+1);
+            }
         }
+
+
+//        System.out.print("Kas soovid ise alustada? Vasta Jah või Ei: ");
+//        Scanner info = new Scanner(System.in);
+//
+
+
+
+//        String answer = info.nextLine();
+//        System.out.print("\n");
+//        if (answer.toLowerCase().equals("jah")) {
+//            System.out.println(name + " alustab mängu!");
+//        } else if (answer.toLowerCase().equals("ei")) {
+//            System.out.println("Sisesta mängija nimi kes võiks alustada!");
+//            String vastus =info.next();
+//            System.out.println(name + " alustab mängu!");
+//            //TODO Vb võtta välja see kes ütles ei
+//        } else {
+//            System.out.println("Pole sobiv vastus");
+//            whoBegins(players);
+//        }
     }
 
     public void turn() {
-        System.out.println();
+        System.out.println("");
     }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,20 +1,37 @@
-public class Player {
+import java.util.Comparator;
+
+public class Player implements Comparable {
 
     private String name;
-    private int score = 0;
+    private int score;
+    private int queue;
 
     public String getName() {
         return name;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getQueue() {
+        return queue;
+    }
+
+    public void setQueue(int queue) {
+        this.queue = queue;
     }
 
     public int getScore() {
         return score;
     }
 
-    public Player(String name, int score) {
+    public Player(String name, int score, int queue) {
         this.name = name;
         this.score = score;
+        this.queue = queue;
     }
+
 
     public void addToScore(int roll) {
         score += roll;
@@ -23,6 +40,20 @@ public class Player {
 
     public void restartScore() {
         score = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                ", queue=" + queue +
+                '}';
+    }
+
+    public int compareTo(Object a) {
+        int compareQueue = ((Player)a).getQueue();
+        return this.queue-compareQueue;
     }
 
 //    public int veeretus() {
